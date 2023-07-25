c821eee19a0e56a020bd01ca29c1ab89c794e389
MUNAZIR <60435223+Munazirraza@users.noreply.github.com>
2023-07-25 23:16:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,81 +1,168 @@
-// Wait for the DOM to load
-document.addEventListener("DOMContentLoaded", () => {
-  // Get the required elements from the DOM
-  const submitButton = document.getElementById("submit");
-  const message = document.querySelector(".message");
-  const cells = document.querySelectorAll(".cell");
+// Start Game events and data changes
+const button = document.querySelectorAll('div>button');
+var player1, player2;
+button[0].addEventListener('click', submitFunc);
 
-  // Add event listener to the submit button
-  submitButton.addEventListener("click", startGame);
+function submitFunc() {
+    const player = document.querySelectorAll('div>input');
+    
+    player1 = player[0].value;
+    player2 = player[1].value;
+    if(player1&&player2) {
+        const inputCont = document.getElementById("input-cont");
+        const gridCont = document.getElementById("grid-cont");
+        inputCont.style.display = 'none';
+        gridCont.style.display = 'flex';
 
-  // Function to handle game start
-  function startGame() {
-    // Get the player names from the input fields
-    const player1Name = document.getElementById("player-1").value;
-    const player2Name = document.getElementById("player-2").value;
-
-    // Hide the player input and show the game board
-    document.getElementById("player-input").style.display = "none";
-    document.getElementById("game-board").style.display = "block";
-
-    // Initialize the game state and active player
-    let gameActive = true;
-    let activePlayer = 1;
-    message.textContent = `${player1Name}, you're up!`;
-
-    // Add event listener to each cell
-    cells.forEach((cell) => {
-      cell.addEventListener("click", () => {
-        // Check if the cell is empty and the game is active
-        if (cell.textContent === "" && gameActive) {
-          // Assign the current player's symbol to the cell
-          cell.textContent = activePlayer === 1 ? "X" : "O";
-          // Check for a win condition
-          if (checkWin()) {
-            message.textContent = `${activePlayer === 1 ? player1Name : player2Name}, congratulations, you won!`;
-            gameActive = false;
-          } else if (checkDraw()) {
-            message.textContent = "It's a draw!";
-            gameActive = false;
-          } else {
-            // Switch the active player
-            activePlayer = activePlayer === 1 ? 2 : 1;
-            message.textContent = `${activePlayer === 1 ? player1Name : player2Name}, you're up!`;
-          }
-        }
-      });
-    });
-
-    // Function to check for a win condition
-    function checkWin() {
-      // Define the winning combinations
-      const winCombinations = [
-        [1, 2, 3], [4, 5, 6], [7, 8, 9], // Rows
-        [1, 4, 7], [2, 5, 8], [3, 6, 9], // Columns
-        [1, 5, 9], [3, 5, 7] // Diagonals
-      ];
-
-      // Check if any winning combination is present on the board
-      for (const combination of winCombinations) {
-        const [a, b, c] = combination;
-        if (cells[a - 1].textContent !== "" &&
-            cells[a - 1].textContent === cells[b - 1].textContent &&
-            cells[a - 1].textContent === cells[c - 1].textContent) {
-          return true; // Winning combination found
-        }
-      }
-
-      return false; // No winning combination
+        gameStart();
     }
-
-    // Function to check for a draw condition
-    function checkDraw() {
-      for (const cell of cells) {
-        if (cell.textContent === "") {
-          return false; // Empty cell found, game still in progress
-        }
-      }
-      return true; // All cells filled, game is a draw
+    else {
+        alert('Please enter both the player names');
     }
-  }
-});
+}
+
+function gameStart() {
+    const subHeading = document.getElementsByClassName('message');
+    
+    subHeading[0].innerText = player1+", you're up";
+}
+
+var turn = 0;
+var gameOn = true;
+var count = 0;
+
+const one = document.getElementById('1');
+const two = document.getElementById('2');
+const three = document.getElementById('3');
+const four = document.getElementById('4');
+const five = document.getElementById('5');
+const six = document.getElementById('6');
+const seven = document.getElementById('7');
+const eight = document.getElementById('8');
+const nine = document.getElementById('9');
+
+one.addEventListener('click', placeYourPick);
+two.addEventListener('click', placeYourPick);
+three.addEventListener('click', placeYourPick);
+four.addEventListener('click', placeYourPick);
+five.addEventListener('click', placeYourPick);
+six.addEventListener('click', placeYourPick);
+seven.addEventListener('click', placeYourPick);
+eight.addEventListener('click', placeYourPick);
+nine.addEventListener('click', placeYourPick);
+
+
+function placeYourPick(event) {
+    if(count!=9&&gameOn) {
+        let playerMark = 'o';
+        if(turn==0) {
+            playerMark = 'x'
+        }
+        event.srcElement.innerText = playerMark;
+        count++;
+		const subHeading = document.getElementsByClassName('message');
+        let won = checkForWin();
+        if(won) {
+            gameOn = false;            
+            if(turn) {
+                subHeading[0].innerText = player2+" congratulations you won!";
+            }
+            else {
+                subHeading[0].innerText = player1+" congratulations you won!";           
+            }
+        }
+		else {
+			
+			if(turn) {
+				subHeading[0].innerText = player1+", you're up";
+			}
+			else subHeading[0].innerText = player2+", you're up";
+		}
+        if(turn) {
+            turn = 0;
+        }
+        else turn++;
+    }
+    else {
+        const subHeading = document.getElementsByClassName('message');
+        subHeading[0].innerText = "Game Completed, click on restart for a new game";        
+    }
+}
+function checkForWin() {
+    if(one.innerText=='x'&&two.innerText=='x'&&three.innerText=='x') {
+        return true;
+    }
+    if(one.innerText=='x'&&four.innerText=='x'&&seven.innerText=='x') {
+        return true;
+    }
+    if(four.innerText=='x'&&five.innerText=='x'&&six.innerText=='x') {
+        return true;
+    }
+    if(seven.innerText=='x'&&eight.innerText=='x'&&nine.innerText=='x') {
+        return true;
+    }
+    if(five.innerText=='x'&&two.innerText=='x'&&eight.innerText=='x') {
+        return true;
+    }
+    if(six.innerText=='x'&&nine.innerText=='x'&&three.innerText=='x') {
+        return true;
+    }
+    if(one.innerText=='x'&&five.innerText=='x'&&nine.innerText=='x') {
+        return true;
+    }
+    if(three.innerText=='x'&&five.innerText=='x'&&seven.innerText=='x') {
+        return true;
+    }
+    
+    if(one.innerText=='o'&&two.innerText=='o'&&three.innerText=='o') {
+        return true;
+    }
+    if(one.innerText=='o'&&four.innerText=='o'&&seven.innerText=='o') {
+        return true;
+    }
+    if(four.innerText=='o'&&five.innerText=='o'&&six.innerText=='o') {
+        return true;
+    }
+    if(seven.innerText=='o'&&eight.innerText=='o'&&nine.innerText=='o') {
+        return true;
+    }
+    if(five.innerText=='o'&&two.innerText=='o'&&eight.innerText=='o') {
+        return true;
+    }
+    if(six.innerText=='o'&&nine.innerText=='o'&&three.innerText=='o') {
+        return true;
+    }
+    if(one.innerText=='o'&&five.innerText=='o'&&nine.innerText=='o') {
+        return true;
+    }
+    if(three.innerText=='o'&&five.innerText=='o'&&seven.innerText=='o') {
+        return true;
+    }
+    return false;
+}
+
+button[1].addEventListener('click', clearBoard);
+button[2].addEventListener('click',restartGame);
+
+function clearBoard() {
+    count = 0;
+    one.innerText = '';
+    two.innerText = '';
+    three.innerText = '';
+    four.innerText = '';
+    five.innerText = '';
+    six.innerText = '';
+    seven.innerText = '';
+    eight.innerText = '';
+    nine.innerText = '';
+    turn = 0;
+    gameOn = true;
+}
+
+function restartGame() {
+    const inputCont = document.getElementById("input-cont");
+    const gridCont = document.getElementById("grid-cont");
+    inputCont.style.display = 'block';
+    gridCont.style.display = 'none';
+    clearBoard();
+}
